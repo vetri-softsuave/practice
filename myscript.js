b6f815b7d3ae3f185696d58b244c97db4d1f911f
vetri-softsuave <vetrivel.softsuave@gmail.com>
2022-10-28 19:36:02 +0530

    28-10-22

---
+++ myscript.js
@@ -0,0 +1,10 @@
+console.log("Hello World!");
+const out=document.getElementById("out");
+document.write("Hello Everyone!");
+
+var str = "haii i am vetrivel and i am software engineer";
+console.log(str.replace("vetrivel","puviarasu"));
+arr = [1,2,3,4];
+console.log(typeof(arr));
+console.log(str.split("v"));
+console.log(parseInt(23.9));
