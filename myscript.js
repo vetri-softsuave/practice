00e96b2b0a1a63a87810914b5b028f7c76a414e4
vetri-softsuave <vetrivel.softsuave@gmail.com>
2022-11-03 19:32:29 +0530

    simpleAnimation added

--- myscript.js
+++ myscript.js
@@ -1,19 +1,18 @@
-console.log("Hello World!");
-const out=document.getElementById("out");
-document.write("Hello Everyone!");
+class person{
+    name = "vetri"
+    print(){
+        console.log("welcome vetri");
+    }
+}
 
-var str = "haii i am vetrivel and i am software engineer";
-console.log(str.replace("vetrivel","puviarasu"));
-arr = [1,2,3,4];
-console.log(typeof(arr));
-console.log(str.split("v"));
-console.log(parseInt(23.9));
-document.write("<br>hello vetri");
-const nums=["one","two","three","four"];
+class employee extends person{
+    position  = "software engineer";
 
-obj={
-    "apple":200,
-    "orange":300,
-    1:"one"
-};
-console.log(obj.values())
+    print(){
+        super.print();
+        console.log("bye vetri");
+    }
+}
+const obj = new employee();
+console.log("employeee name "+obj.name);
+obj.print();
