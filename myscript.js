83cbf3756f2bd04aa5adb50e9608a3056b060af2
vetri-softsuave <vetrivel.softsuave@gmail.com>
2022-11-05 19:22:40 +0530

    05 nov

--- myscript.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector(".btn");
-const out = document.getElementById("out");
-
-btn.addEventListener("click", change);
-
-function change() {
-  out.innerHTML = "welcome vetri";
-}
